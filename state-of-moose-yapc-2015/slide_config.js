867a42a64e124181f6eb2f40024fd9bfa9033f97
Dave Rolsky <autarch@urth.org>
2015-07-27 16:44:58 -0500

    Add State of Moose from YAPC 2015

---
+++ state-of-moose-yapc-2015/slide_config.js
@@ -0,0 +1,29 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'State of the Velocimoose 2015',
+    subtitle: 'A Hastily Prepared Talk That Sawyer Told Me I Was Doing Last Night. Seriously! While we were walking to dinner! I mean, what does he expect? Well, this is what he gets, and he darn well better like it.',
+    eventInfo: {
+      title: 'YAPC::NA::2015',
+      date: '2015-06-08'
+    },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    favIcon: 'images/red-butte-moose-icon.jpg',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Dave Rolsky',
+    company: 'Sometimes Moose Wrangler',
+    github: 'http://github.com/autarch'
+  }]
+};
+
