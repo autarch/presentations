6ed0e92f6638245a3368bd4ae30b73d3a163d8f7
Dave Rolsky <autarch@urth.org>
2020-09-20 17:29:56 -0500

    Add shell script for render.com build

---
+++ render.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+set -x
+
+find -type l -name node_modules -exec rm '{}' \;
+
+mkdir p
+rsync \
+    --exclude '**/Gruntfile.js' \
+    --exclude '**/gulpfile.js' \
+    --exclude '**/package.json' \
+    --exclude '.git/**' \
+    --exclude '.venv/**' \
+    --exclude 'node_modules/**' \
+    --exclude 'p/**' \
+    --exclude 'reveal.js' \
+    --exclude 'reveal4' \
+    -a --copy-links --delete \
+    ./ ./p/
